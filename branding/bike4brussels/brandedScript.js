3e3b2029204cc17387f95ee62eadb45a4e474cd1
xivk <ben@itinero.tech>
2019-12-19 12:27:39 +0000

    Make shields adapt to text length.

--- branding/bike4brussels/brandedScript.js
+++ branding/bike4brussels/brandedScript.js
@@ -194,7 +194,7 @@ branding.prototype.addLayers = function(map) {
         "minzoom": 10,
         "maxzoom": 24,
         "layout": {
-          "icon-image": "us-state_{ref_length",
+          "icon-image": "us-state_{ref_length}",
           "icon-rotation-alignment": "viewport",
           "icon-size": 1,
           "symbol-placement": {
