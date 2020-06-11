4cda430f747aa71935931c4c79eee33422550146
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-06-11 13:39:12 +0200

    Use 'staging' for develop branch

--- index.ts
+++ index.ts
@@ -34,7 +34,7 @@ const production_urls = {
     mapStyle: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
     network: 'https://cyclenetworks.osm.be/brumob/data/network.geojson',
     //route: 'https://cycling-backend.anyways.eu/api',
-    route: 'https://routing.anyways.eu/api',
+    route: 'https://staging.anyways.eu/routing-api/',
     geocoder: `https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?` +
         `access_token=${mapboxAccessCode}&proximity=5.5196%2c50.9612` +
         'country=BE&' +
@@ -74,7 +74,7 @@ var initialMap = {
 var urls = {
     mapStyle: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
     network: 'https://cyclenetworks.osm.be/brumob/data/network.geojson',
-    route: 'https://routing.anyways.eu/api/route?',
+    route: 'https://staging.anyways.eu/routing-api/route?',
     geocoder: `https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?` +
         `access_token=${mapboxAccessCode}&proximity=5.5196%2c50.9612` +
         'country=BE&' +
