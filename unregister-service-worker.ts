1b763e08acf94c28075e34d3920ea215de7dc651
xivk <ben@itinero.tech>
2020-12-07 18:07:55 +0100

    Force service workers to unregister.

---
+++ unregister-service-worker.ts
@@ -0,0 +1,12 @@
+export class ServiceWorkerHelper {
+    static UnregisterAll(): void {
+        if(window.navigator && navigator.serviceWorker) {
+            navigator.serviceWorker.getRegistrations()
+            .then(function(registrations) {
+              for(let registration of registrations) {
+                registration.unregister();
+              }
+            });
+          }
+    }
+}
